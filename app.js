c71e497d9fbb0baf3bdac95deb081e1ae8fc9042
yavehreyes <yavehreyes@outlook.com>
2021-11-19 21:58:03 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function mobileAction() {
-	const mobileIcon = document.
+	document.querySelector('.main').style.display = 'none';
+	document.querySelector('.footer').style.display = 'none';
+	document.querySelector('.mobile-menu').style.display = 'none';
+	document.querySelector('.menu-lv').style.display = 'block';
+	document.querySelector('.mobile-nav').style.display = 'block';
+	document.querySelector('.mobile-link').style.display = 'block';
+}
+function mobileOff() {
+	document.querySelector('.mobile-menu').style.display = 'block';
+	document.querySelector('.main').style.display = 'block';
+	document.querySelector('.footer').style.display = 'block';
+	document.querySelector('.navbar').style.display = 'block';
+	document.querySelector('.menu-lv').style.display = 'none';
+	document.querySelector('.mobile-nav').style.display = 'none';
 }
